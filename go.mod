f8354c2769e38db5f641e7bdd2fb9e658eea8236
Mike Rybka <merybka@gmail.com>
2024-05-17 18:36:34 -0400

    wip

---
+++ go.mod
@@ -0,0 +1,15 @@
+module github.com/brass-software/server
+
+go 1.22.1
+
+require github.com/mikerybka/util v0.0.0-20240517211702-ba8d543e24ee
+
+require (
+	github.com/fsnotify/fsnotify v1.7.0 // indirect
+	github.com/kr/fs v0.1.0 // indirect
+	github.com/pkg/sftp v1.13.6 // indirect
+	golang.org/x/crypto v0.15.0 // indirect
+	golang.org/x/net v0.18.0 // indirect
+	golang.org/x/sys v0.14.0 // indirect
+	golang.org/x/text v0.14.0 // indirect
+)
---
+++ main.go
@@ -0,0 +1,60 @@
+package main
+
+import (
+	"net/http"
+	"os"
+
+	"github.com/mikerybka/util"
+)
+
+func main() {
+	email := os.Getenv("EMAIL")
+	if email == "" {
+		panic("missing EMAIL")
+	}
+	certDir := os.Getenv("CERT_DIR")
+	if certDir == "" {
+		panic("missing CERT_DIR")
+	}
+	twilioAccountSID := os.Getenv("TWILIO_ACCOUNT_SID")
+	if twilioAccountSID == "" {
+		panic("missing TWILIO_ACCOUNT_SID")
+	}
+	twilioAuthToken := os.Getenv("TWILIO_AUTH_TOKEN")
+	if twilioAuthToken == "" {
+		panic("missing TWILIO_AUTH_TOKEN")
+	}
+	twilioPhoneNumber := os.Getenv("TWILIO_PHONE_NUMBER")
+	if twilioPhoneNumber == "" {
+		panic("missing TWILIO_PHONE_NUMBER")
+	}
+	authDir := os.Getenv("AUTH_DIR")
+	if authDir == "" {
+		authDir = "data/auth"
+	}
+	dataDir := os.Getenv("SCHEMA_CAFE_DIR")
+	if dataDir == "" {
+		dataDir = "data/schema.cafe"
+	}
+
+	s := &util.MultiHostServer{
+		Hosts: map[string]http.Handler{
+			"api.schema.cafe": &util.MultiUserApp{
+				Twilio: &util.TwilioClient{
+					AccountSID:  twilioAccountSID,
+					AuthToken:   twilioAuthToken,
+					PhoneNumber: twilioPhoneNumber,
+				},
+				AuthFiles: &util.LocalFileSystem{
+					Root: authDir,
+				},
+				App: &util.SchemaCafe{
+					Data: &util.LocalFileSystem{
+						Root: dataDir,
+					},
+				},
+			},
+		},
+	}
+	panic(s.Start(email, certDir))
+}
