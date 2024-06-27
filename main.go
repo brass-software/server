f4d9e1e04212262ee7a58b03f12382bdb5fd4e10
Mike Rybka <merybka@gmail.com>
2024-06-27 13:49:33 -0400

    wip

--- main.go
+++ main.go
@@ -3,12 +3,17 @@ package main
 import "github.com/mikerybka/util"
 
 func main() {
-	twilioClient := &util.TwilioClient{
-		AccountSID:  util.RequireEnvVar("TWILIO_ACCOUNT_SID"),
-		AuthToken:   util.RequireEnvVar("TWILIO_AUTH_TOKEN"),
-		PhoneNumber: util.RequireEnvVar("TWILIO_PHONE_NUMBER"),
+	server := &util.Server{
+		DataDir: util.EnvVar("DATA_DIR", "/root"),
+		TwilioClient: &util.TwilioClient{
+			AccountSID:  util.RequireEnvVar("TWILIO_ACCOUNT_SID"),
+			AuthToken:   util.RequireEnvVar("TWILIO_AUTH_TOKEN"),
+			PhoneNumber: util.RequireEnvVar("TWILIO_PHONE_NUMBER"),
+		},
+		AdminPhone: util.RequireEnvVar("ADMIN_PHONE"),
+		AdminEmail: util.RequireEnvVar("ADMIN_EMAIL"),
+		CertDir:    util.RequireEnvVar("CERT_DIR"),
 	}
-	server := util.NewServer(util.EnvVar("DATA_DIR", "/root"), util.RequireEnvVar("ADMIN_PHONE"), twilioClient)
-	err := server.Start(util.RequireEnvVar("ADMIN_EMAIL"), util.RequireEnvVar("CERT_DIR"))
+	err := server.Start()
 	panic(err)
 }
